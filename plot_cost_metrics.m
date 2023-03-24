6b99047abc50216835dd092f2bee1574b648e221
MohamedNaveed <mohdnaveed96@gmail.com>
2023-03-24 12:52:17 -0500

    sweep over T added

---
+++ plot_cost_metrics.m
@@ -0,0 +1,21 @@
+function [] = plot_cost_metrics(T_vec, cost_ilqr,...
+                            total_cost, exp_CTG_vec, true_CTG_vec)
+
+                        
+figure;
+plot(T_vec, total_cost,'b','LineWidth',3, 'DisplayName', 'Total cost');
+hold on;
+plot(T_vec, cost_ilqr,'r--','LineWidth',2, 'DisplayName', 'iLQR cost');
+xlabel('Finite horizon T');
+ylabel('Cost');
+legend();
+                        
+figure;
+plot(T_vec, exp_CTG_vec,'b','LineWidth',3, 'DisplayName', 'Expected CTG');
+hold on;
+plot(T_vec, true_CTG_vec,'r--','LineWidth',2, 'DisplayName', 'Actual CTG');
+xlabel('Finite horizon T');
+ylabel('Cost');
+legend();
+end
+
