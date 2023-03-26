9c896f953c88d9ec60348ab9f5d994eda0deb3cd
Mohamed Naveed <mohdnaveed96@gmail.com>
2023-03-26 10:38:05 -0500

    Cartpole error fixed. Working fine.

--- plot_cost_metrics.m
+++ plot_cost_metrics.m
@@ -3,7 +3,7 @@ function [] = plot_cost_metrics(T_vec, cost_ilqr,...
 
                         
 figure;
-plot(T_vec, total_cost,'b','LineWidth',3, 'DisplayName', 'Total cost');
+plot(T_vec, total_cost,'b','Marker','o','LineWidth',3, 'DisplayName', 'Total cost');
 hold on;
 plot(T_vec, cost_ilqr,'r--','LineWidth',2, 'DisplayName', 'iLQR cost');
 xlabel('Finite horizon T');
@@ -11,7 +11,7 @@ ylabel('Cost');
 legend();
                         
 figure;
-plot(T_vec, exp_CTG_vec,'b','LineWidth',3, 'DisplayName', 'Expected CTG');
+plot(T_vec, exp_CTG_vec,'b','Marker','o','LineWidth',3, 'DisplayName', 'Expected CTG');
 hold on;
 plot(T_vec, true_CTG_vec,'r--','LineWidth',2, 'DisplayName', 'Actual CTG');
 xlabel('Finite horizon T');
