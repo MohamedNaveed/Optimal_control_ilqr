347f08e74d13fca1abd4134dce11f98190ccb4ab
MohamedNaveed <mohdnaveed96@gmail.com>
2024-02-08 15:53:40 -0600

    mpc sample traj gen added

--- mpc_main.m
+++ mpc_main.m
@@ -22,47 +22,52 @@ maxIte = 200;
 
 %% MPC using ilqr solver
 
-%
-load('data/u_guess_1dcos_T200.mat');
-%u_guess = zeros(model.nu, model.horizon);
-%u_nom = u_guess
-cost_mpc = 0;
-
-x_mpc = zeros(model.nx, model.horizon+1);
-x_mpc(:,1) = model.X0;
-
-u_mpc = zeros(model.nu, model.horizon);
-epsilon = 0.1;
+epsilon = 8;
 
 maxIte = 100;
-tic;
-for t = 0:model.horizon-1
+n_samples = 5;
+x_data = zeros(n_samples, model.horizon+1);
+for ns = 1:n_samples
+    x_mpc = zeros(model.nx, model.horizon+1);
+    u_mpc = zeros(model.nu, model.horizon);
+    x_mpc(:,1) = model.X0;
+    load('data/u_guess_1dcos_T200.mat');
+    %u_guess = zeros(model.nu, model.horizon);
+    %u_nom = u_guess
+    cost_mpc = 0;
     
-    %fprintf('t= %d \n',t);
-    T = model.horizon - t;
+    tic;
+    for t = 0:model.horizon-1
+        
+        fprintf('t= %d \n',t);
+        T = model.horizon - t;
+        
+        u_guess = u_nom(:,end-T+1:end);
+        
+        [x_nom, u_nom, cost] = ILQR(model, x_mpc(:,t+1), model.Xg, u_guess, T,...
+                                model.Q, model.R, model.Qf, maxIte); %trajectory optimization using iLQR.
+        
+        u_mpc(:,t+1) = u_nom(:,1);
+        
+        state_err = compute_state_error(x_nom(:,1), model.Xg, model.name);
     
-    u_guess = u_nom(:,end-T+1:end);
-    
-    [x_nom, u_nom, cost] = ILQR(model, x_mpc(:,t+1), model.Xg, u_guess, T,...
-                            model.Q, model.R, model.Qf, maxIte); %trajectory optimization using iLQR.
-    
-    u_mpc(:,t+1) = u_nom(:,1);
-    
-    state_err = compute_state_error(x_nom(:,1), model.Xg, model.name);
+        cost_mpc = cost_mpc + (0.5*state_err'*model.Q*state_err + ... 
+                                        0.5*u_mpc(:,t+1)'*model.R*u_mpc(:,t+1)); %incremental cost
+                                    
+        x_mpc(:,t+2) = model.state_prop(t+1, x_mpc(:,t+1), u_mpc(:,t+1), model, epsilon); % state propagate.
+     
+    end
+    x_data(ns,:) = x_mpc;
 
-    cost_mpc = cost_mpc + (0.5*state_err'*model.Q*state_err + ... 
-                                    0.5*u_mpc(:,t+1)'*model.R*u_mpc(:,t+1)); %incremental cost
-                                
-    x_mpc(:,t+2) = model.state_prop(t+1, x_mpc(:,t+1), u_mpc(:,t+1), model, epsilon); % state propagate.
- 
+    time_taken = toc;
+    state_err = compute_state_error(x_mpc(:,model.horizon+1), model.Xg, model.name);
+    cost_mpc = cost_mpc + (0.5*state_err'*model.Qf*state_err); %terminal cost
+    fprintf('Time taken %d \n', time_taken);
 end
-time_taken = toc;
-state_err = compute_state_error(x_mpc(:,model.horizon+1), model.Xg, model.name);
-cost_mpc = cost_mpc + (0.5*state_err'*model.Qf*state_err); %terminal cost
-fprintf('Time taken %d \n', time_taken);
+
 
 %% 
-plot_trajectory(x_mpc, u_mpc, model.horizon,0,model.name,model.dt);
+%plot_trajectory(x_mpc, u_mpc, model.horizon,0,model.name,model.dt);
 %%
 %{
 load('/home/naveed/Documents/Dynamic_programming/Data/dp_hjb_1dcos_n_200_N_300000_epsi0_1.mat');
@@ -85,3 +90,27 @@ ylabel('u');
 xlabel('Time steps');
 legend();
 %}
+
+%% plot ensemble of trajectories
+
+fig = figure;
+timesteps = (0:model.horizon)*model.dt;
+plot(timesteps, x_data,'LineWidth',2);
+ylabel('state');
+xlabel('time');
+grid on;
+ylim([-3,3]);
+SAVE_PLOT = true;
+if SAVE_PLOT
+    font_size = 16;
+    ax = gca;
+    ax.FontSize = font_size; 
+    %set(h,'FontSize',font_size);
+    set(fig,'Units','inches');
+    screenposition = get(fig,'Position');
+    set(fig,...
+        'PaperPosition',[0 0 screenposition(3:4)],...
+        'PaperSize',[screenposition(3:4)]);
+    
+    print -dpdf -vector 'mpc_ilqr_traj_epsi8.pdf'
+end
