ac66340897f81008fa63871ded20776b0726f8d7
MohamedNaveed <mohdnaveed96@gmail.com>
2024-02-12 16:58:16 -0600

    tpfc added

--- tpfc_monte_carlo.m
+++ tpfc_monte_carlo.m
@@ -1,8 +1,9 @@
+% TPFC with ILQR.
 % mpc using ilqr
 
 clear;clc;
 
-model = model_register('car');
+model = model_register('cartpole');
 model.q = 1; %full state
 model.name
 fprintf('initial state = %d \n', model.X0);
@@ -11,20 +12,24 @@ fprintf('final state = %d \n', model.Xg);
 fprintf('Horizon = %d \n', model.horizon);
 fprintf('dt = %d \n', model.dt);
 
-SAVE_MAT_FILE = false;
 WRITE_CSVFILE = false; 
-output_filename = 'mpc_car_e001_02_T30.csv';
+output_filename = 'tpfc_cartpole_e001_02_T30.csv';
 header = ["epsilon", "Average Cost", "Cost variance", "Time taken"];
 writematrix(header,output_filename,'WriteMode','append');
 
-epsi_range = [0.01,0.02,0.03,0.04,0.05,0.1,0.2];
+epsi_range = [0.023,0.024];
 
 n_mc_runs = 500;
 
 cost_vec = zeros(length(epsi_range),1);
 var_vec = zeros(length(epsi_range),1);
 
-num_cores = 1;
+load('data/cartpole_init_guess_T30.mat');
+maxIte = 100;
+u_guess = u_nom;
+
+[x_nom, u_nom, cost, K] = ILQR(model, model.X0, model.Xg, u_guess, model.horizon,...
+                            model.Q, model.R, model.Qf, maxIte); %trajectory optimization using iLQR.
 
 for it = 1:length(epsi_range)
     
@@ -34,7 +39,7 @@ for it = 1:length(epsi_range)
     %parfor (n_mc = 1:n_mc_runs, num_cores)
     for (n_mc = 1:n_mc_runs)
         n_mc
-        cost_vec_mc(n_mc) = mpc_ilqr(model, epsilon);
+        cost_vec_mc(n_mc) = tpfc_ilqr(model, epsilon, x_nom, u_nom, K);
     end
  
     time_taken = toc;
@@ -46,9 +51,7 @@ for it = 1:length(epsi_range)
     
 end
 
-if SAVE_MAT_FILE    
-    save("mpc_car_e02_04_T200.mat");
-end
+
 
 
 
