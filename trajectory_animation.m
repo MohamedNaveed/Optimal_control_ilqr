bf77b49ab66a120d2a19aa7b59f704c2d25dbd27
naveed <mohdnaveed96@gmail.com>
2024-06-17 10:57:06 -0500

    added gifs

--- trajectory_animation.m
+++ trajectory_animation.m
@@ -1,21 +1,12 @@
 clc;
 clear;
-load('data/traj_iterations_car_wobs.mat');
+load('data/traj_iterations_car.mat');
 
 % Ellipse parameters
-%c_obs_1 = [5; 1; 0; 0];
 c_obs_1 = [2.25; 3]; % Ellipse center 1
 c_obs_2 = [17.75; 3]; % Ellipse center 2
 E_obs_1 = [(1/2.5)^2, 0; 0, 1]; % Parameters of the ellipse
 E_obs_2 = E_obs_1; % Same parameters for the second ellipse
-
-% Initialize video writer
-videoFile = 'trajectory_animation_wobs.avi';
-v = VideoWriter(videoFile, 'Motion JPEG AVI');
-v.FrameRate = 10; % Set frame rate
-%v.Quality = 95; % Set higher quality
-open(v);
-
 
 fig = figure('Visible', 'off'); % Create figure without displaying
 hold on;
@@ -28,14 +19,13 @@ plot(model.X0(1), model.X0(2), 'Marker', '.', 'Color', 'r', ...
 plot(model.Xg(1), model.Xg(2), 'Marker', '.', 'Color', 'g', ...
     'MarkerSize', 15, 'DisplayName', 'Goal');
 
-%Plot ellipses
-
-h1 = plot_ellipse(c_obs_1, E_obs_1, 'k'); % Blue ellipse
+% Plot ellipses
+%{
+h1 = plot_ellipse(c_obs_1, E_obs_1, 'k'); % First ellipse
 set(h1, 'DisplayName', 'Obstacle'); % Set legend entry for the first ellipse
-h2 = plot_ellipse(c_obs_2, E_obs_2, 'k'); % Blue ellipse
-set(h2, 'HandleVisibility', 'off');
-
-
+h2 = plot_ellipse(c_obs_2, E_obs_2, 'k'); % Second ellipse
+set(h2, 'HandleVisibility', 'off'); % Hide legend entry for the second ellipse
+%}
 % Customize plot
 ylim([-1, 5]); % current y-axis limits
 xlim([-1, 20]);
@@ -43,34 +33,37 @@ grid on;
 legend();
 xlabel('$x$ [m]', 'Interpreter', 'latex');
 ylabel('$y$ [m]', 'Interpreter', 'latex');
-ax = findobj(gcf,'type','axes'); % current axes
+ax = findobj(gcf, 'type', 'axes'); % current axes
 set(ax, 'FontSize', 14);
 
 x_nom = x_traj_ite_ilqr(:,:,1);
 % Plot trajectory
 h_traj = plot(x_nom(1,:), x_nom(2,:), 'Marker', '.', 'MarkerSize', 10, ...
-    'LineWidth', 2, 'DisplayName', 'Trajectory', 'Color','b');
+    'LineWidth', 2, 'DisplayName', 'Trajectory', 'Color', 'b');
 
-% Capture frame and write to video
-frame = getframe(fig);
-writeVideo(v, frame);
-% Pause for a short duration to create the animation effect
-pause(0.1);
+% GIF file name
+filename = 'trajectory_animation.gif';
 
-% Animation loop
-for k = 2:size(x_traj_ite_ilqr, 3)
+% Loop to create animation frames and save as GIF
+for k = 1:size(x_traj_ite_ilqr, 3)
     % Update trajectory plot
-    x_nom = x_traj_ite_ilqr(:,:,k);
+    x_nom = x_traj_ite_ilqr(:, :, k);
     set(h_traj, 'XData', x_nom(1, :), 'YData', x_nom(2, :));
     
-    % Capture frame and write to video
+    % Capture frame
     frame = getframe(fig);
-    writeVideo(v, frame);
+    im = frame2im(frame);
+    [imind, cm] = rgb2ind(im, 256);
+    
+    % Write to GIF file
+    if k == 1
+        imwrite(imind, cm, filename, 'gif', 'Loopcount', inf, 'DelayTime', 0.1);
+    else
+        imwrite(imind, cm, filename, 'gif', 'WriteMode', 'append', 'DelayTime', 0.1);
+    end
+    
     % Pause for a short duration to create the animation effect
     pause(0.1);
 end
 
 hold off;
-
-% Close the video file
-close(v);
